6fe2f980d73e46d9c426531293eeec10a9420af9
nickelk <nick_smith@ukr.net>
2021-12-01 17:14:50 +0200

    home page through template view added

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,4 @@
-"""onemorecrm URL Configuration
+"""catalog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -13,10 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import HomePageView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('catalog.urls')),
+    path('', HomePageView.as_view(), name='home'),
 ]
