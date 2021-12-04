fe2eb18d62ac0188aecb302aa0415409feae2b5c
nickelk <nick_smith@ukr.net>
2021-12-04 15:38:20 +0200

    project and interaction models added

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Customer, Phone, Email
+from .models import Customer, Phone, Email, Project, Interaction
 
 admin.site.register(Customer)
 admin.site.register(Phone)
 admin.site.register(Email)
+admin.site.register(Project)
+admin.site.register(Interaction)
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,6 @@
+from datetime import date
+
+from django.contrib.auth.models import User
 from django.db import models
 from ckeditor.fields import RichTextField
 
@@ -9,7 +12,7 @@ class Phone(models.Model):
     """
     Class defining a model, derived from the Model class.
     """
-    phone = models.CharField(max_length=30, help_text="Enter phone number")
+    phone = models.CharField(max_length=30, help_text="Phone number")
 
     def __str__(self):
         """
@@ -22,7 +25,7 @@ class Email(models.Model):
     """
     Class defining a model, derived from the Model class.
     """
-    email = models.EmailField(help_text="Enter email")
+    email = models.EmailField(help_text="Email")
 
     def __str__(self):
         """
@@ -33,16 +36,16 @@ class Email(models.Model):
 
 class Customer(models.Model):
     """
-    Class defining a model, derived from the Model class.
+    Class defining a model of company(customer).
     """
-    company_name = models.CharField(max_length=300, help_text="Enter company name")
-    foreman_name = models.CharField(max_length=300, help_text="Enter foreman's name")
-    descriptions = RichTextField(help_text="Describe the company")
+    company_name = models.CharField(max_length=300, help_text="Company name")
+    foreman_name = models.CharField(max_length=300, help_text="Foreman's name")
+    description = RichTextField(help_text="Describe the company")
     date_of_creation = models.DateField(auto_now_add=True)
     date_of_edition = models.DateField(auto_now=True)
-    adress = models.CharField(max_length=300, help_text="Enter the adress of the organization")
-    phone = models.ManyToManyField(Phone, help_text="Enter phone number")
-    email = models.ManyToManyField(Email, help_text="Enter email")
+    address = models.CharField(null=True, max_length=300, help_text="Address of the organization")
+    phone = models.ManyToManyField(Phone, help_text="Phone number(s)")
+    email = models.ManyToManyField(Email, help_text="Email(s)")
 
     def __str__(self):
         """
@@ -52,6 +55,66 @@ class Customer(models.Model):
 
     def get_absolute_url(self):
         """
-        Returns the url to access a particular book instance.
+        Returns the url to access a particular customer instance.
         """
         return reverse('customer-detail', args=[str(self.id)])
+
+
+class Project(models.Model):
+    """
+    Class defining a model of project.
+    """
+    title = models.CharField(max_length=300, help_text="Project title")
+    description = RichTextField(help_text="Describe the project")
+    begin_date = models.DateField(default=date.today, help_text="Project start date")
+    end_date = models.DateField(null=True, blank=True, help_text="Project end date")
+    price = models.FloatField()
+
+    def __str__(self):
+        """
+        String for representing the Model object
+        """
+        return self.title
+
+
+class Interaction(models.Model):
+    """
+    Class defining a model of interaction between customer and contractor.
+    """
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, null=True)
+    company = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+
+    CHANNEL = (
+        ('r', 'Request'),
+        ('l', 'Letter'),
+        ('w', 'Web site'),
+        ('c', 'Company init.'),
+    )
+
+    channel_of_reference = models.CharField(max_length=1,
+                                            choices=CHANNEL,
+                                            blank=True,
+                                            default='r',
+                                            help_text='Interaction way')
+
+    manager = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    description = RichTextField(help_text="Describe the interaction")
+
+    GRADES = ((-5, '-5'), (-4, '-4'), (-3, '-3'), (-2, '-2'), (-1, '-1'),
+              (1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'))
+
+    grade = models.DecimalField(decimal_places=2,
+                                max_digits=2,
+                                choices=GRADES,
+                                blank=True,
+                                default=1,
+                                help_text='Interaction grade')
+
+    date_of_creation = models.DateField(auto_now_add=True)
+    date_of_edition = models.DateField(auto_now=True)
+
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        return 'Proj.: %s, Comp.: %s' % (self.project, self.company)
