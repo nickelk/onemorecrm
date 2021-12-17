6679ffb588c9d296123c27ac76ceff5ab4df96e5
nickelk <nick_smith@ukr.net>
2021-12-17 07:51:55 +0200

    owncabinet works without pk in URL

--- owncabinet/models.py
+++ owncabinet/models.py
@@ -7,14 +7,8 @@ class OwnCabinet(AbstractUser):
     avatar = models.ImageField(upload_to='avatar_images/',
                                default='avatar_images/default.jpeg')
 
-    def get_profile_update_url(self):
-        """
-        Returns the url to update interaction instance.
-        """
-        return reverse('owncabinet-update', args=[str(self.id)])
-
     def get_absolute_url(self):
         """
         Returns the url to access a particular profile instance.
         """
-        return reverse('owncabinet', args=[str(self.id)])
+        return reverse('owncabinet')
--- owncabinet/views.py
+++ owncabinet/views.py
@@ -1,12 +1,31 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DetailView, UpdateView
 from .models import OwnCabinet
+from  django.shortcuts import get_object_or_404
 
 
 class OwnCabinetDetailView(LoginRequiredMixin, DetailView):
     model = OwnCabinet
+    
+    def setup(self, request, *args, **kwargs):
+        self.user_id = request.user.pk
+        return super(OwnCabinetDetailView, self).setup(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        if not queryset:
+            queryset = self.get_queryset()
+        return get_object_or_404(queryset, pk=self.user_id)
 
 
 class OwnCabinetUpdateView(LoginRequiredMixin, UpdateView):
     model = OwnCabinet
     fields = ['username', 'avatar', 'first_name', 'last_name', 'email']
+
+    def setup(self, request, *args, **kwargs):
+        self.user_id = request.user.pk
+        return super(OwnCabinetUpdateView, self).setup(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        if not queryset:
+            queryset = self.get_queryset()
+        return get_object_or_404(queryset, pk=self.user_id)
