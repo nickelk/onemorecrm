9c9ddfe95fa6137670288171ba5fb6f600184597
nickelk <nick_smith@ukr.net>
2021-12-19 12:31:05 +0200

    customer detailview tests added

--- catalog/tests.py
+++ catalog/tests.py
@@ -88,3 +88,29 @@ class CustomerListViewTest(TestCase):
         self.assertTrue('is_paginated' in resp.context)
         self.assertTrue(resp.context['is_paginated'] == True)
         self.assertTrue(len(resp.context['customer_list']) == 3)
+
+
+class CustomerDetailViewTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = OwnCabinet.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
+        Customer.objects.create(company_name='Big Company', foreman_name='Bob Dylan')
+
+    def test_view_url_exists_at_desired_location(self):
+        self.client.login(username='john', password='johnpassword')
+        resp = self.client.get('/customer/1')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        self.client.login(username='john', password='johnpassword')
+        resp = self.client.get(reverse('customer-detail', args='1'))
+        print(resp)
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        self.client.login(username='john', password='johnpassword')
+        resp = self.client.get(reverse('customer-detail', args='1'))
+        self.assertEqual(resp.status_code, 200)
+
+        self.assertTemplateUsed(resp, 'catalog/customer_detail.html')
